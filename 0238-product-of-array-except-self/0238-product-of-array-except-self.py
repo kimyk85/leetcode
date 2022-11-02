b0713be277faafe8d16e565d63f962ad598f3e84
kimyk85 <105425476+kimyk85@users.noreply.github.com>
2022-11-03 00:15:24 +0900

    Time: 576 ms (39.68%), Space: 21.7 MB (33.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,7 +3,6 @@ class Solution:
         return_list = []
         left = 1
         right = 1
-        left_list = []
         right_list = []
 
         for i in range(len(nums) -1 , 0,  -1):
@@ -15,6 +14,5 @@ class Solution:
         return_list.append(right_list[0])
         for i in range(0, len(nums)-1):
             left = left * nums[i]
-            left_list.append(left)
             return_list.append(left * right_list[i+1])
         return return_list
