1a4e215991b5682e4abffaf4453b4d0c6abf3d0a
kimyk85 <105425476+kimyk85@users.noreply.github.com>
2022-11-02 23:24:18 +0900

    Time: 606 ms (7.56%), Space: 18.4 MB (83.15%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        #a = s[::-1]
+        #s = a
+        s.reverse()
